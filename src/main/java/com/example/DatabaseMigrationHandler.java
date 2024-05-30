bbfa9849aabcd6f5a7b9a76ccc3040f8ebf01960
nbekenov <nur.beken.usa@gmail.com>
2024-05-29 20:56:34 -0400

    working version with class constructor

--- src/main/java/com/example/DatabaseMigrationHandler.java
+++ src/main/java/com/example/DatabaseMigrationHandler.java
@@ -5,42 +5,55 @@ import org.flywaydb.core.Flyway;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Objects;
 import software.amazon.jdbc.PropertyDefinition;
 import software.amazon.jdbc.ds.AwsWrapperDataSource;
 
 public class DatabaseMigrationHandler {
+    // instance vars
+    private final String dbHost;
+    private final String dbPort;
+    private final String dbName;
+    private final String dbSchema;
+    private final String dbUser;
+    private final String dbPassword;
 
-    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_HOST = "localhost";
+    private static final String DB_PORT = "5432";
+    private static final String DB_NAME = "postgres";
+    private static final String DB_SCHEMA = "myschema";
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "mysecretpassword";
 
-//    private final String host;
-//    private final String port;
-//    private final String user;
-//    private final String database;
-//    private final String dbSchema;
-//
-//    public DatabaseMigrationHandler() {
-//        this.host = "jdbc:postgresql://db:5432/postgres";
-//        this.port = "5432";
-//        this.user = "postgres";
-//        this.database = "postgres";
-//        this.dbSchema = "postgres";
-//    }
+    // Constructor
+    public DatabaseMigrationHandler() {
+        this.dbHost = DB_HOST;
+        this.dbPort = DB_PORT;
+        this.dbName = DB_NAME;
+        this.dbSchema = DB_SCHEMA;
+        this.dbUser = DB_USER;
+        this.dbPassword = DB_PASSWORD;
+    }
+
+    private String getSystemProperty(String propName){
+        String err_msg = "Required system property '" + propName + "' not found";
+        return Objects.requireNonNull(System.getProperty(propName), err_msg);
+    }
 
     public static void main(String[] args) {
+        DatabaseMigrationHandler handler = new DatabaseMigrationHandler();
         // Test database connection
-        if (testConnection()) {
+        if (handler.testConnection()) {
             System.out.println("Database connection successful!");
 
             // Run Flyway migrations
-            runMigrations();
+            handler.runMigrations();
         } else {
             System.out.println("Failed to connect to the database.");
         }
     }
 
-    private static boolean testConnection() {
+    private boolean testConnection() {
         try (Connection connection = getDataSource().getConnection()) {
             return connection != null;
         } catch (SQLException e) {
@@ -49,11 +62,11 @@ public class DatabaseMigrationHandler {
         }
     }
 
-    private static void runMigrations() {
+    private void runMigrations() {
         try{
             Flyway flyway = Flyway.configure()
                     .dataSource(getDataSource())
-                    .schemas("myschema")
+                    .schemas(this.dbSchema)
                     .load();
             flyway.migrate();
             System.out.println("Completed Database migration!");
@@ -63,18 +76,18 @@ public class DatabaseMigrationHandler {
         }
     }
 
-    private static AwsWrapperDataSource getDataSource() {
+    private AwsWrapperDataSource getDataSource() {
         Properties targetDataSourceProps = new Properties();
         targetDataSourceProps.setProperty("ssl", "false");
-        targetDataSourceProps.setProperty("password", DB_PASSWORD);
+        targetDataSourceProps.setProperty("password", this.dbPassword);
 
         AwsWrapperDataSource ds = new AwsWrapperDataSource();
         ds.setJdbcProtocol("jdbc:postgresql:");
         ds.setTargetDataSourceClassName("org.postgresql.ds.PGSimpleDataSource");
-        ds.setServerName("localhost");
-        ds.setDatabase("postgres");
-        ds.setServerPort("5432");
-        ds.setUser("postgres");
+        ds.setServerName(this.dbHost);
+        ds.setDatabase(this.dbName);
+        ds.setServerPort(this.dbPort);
+        ds.setUser(this.dbUser);
         ds.setTargetDataSourceProperties(targetDataSourceProps);
 
         return ds;
